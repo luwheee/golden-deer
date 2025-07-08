b263f64c6087325ad28fe2e170956800a1ffc6d3
luwheee <louiebartolome777@gmail.com>
2025-07-08 11:04:54 +0800

    Update champion_tracker.py

--- champion_tracker.py
+++ champion_tracker.py
@@ -103,7 +103,7 @@ recruitment_points = {
     "Successful Final Interview": 5
 }
 # Logo from Google Drive
-st.image("https://drive.google.com/file/d/13aAGZz1lnnre-de8fUUypHeIyi4Zy1uD/view", width=120)
+st.image("https://drive.google.com/file/d/13aAGZz1lnnre-de8fUUypHeIyi4Zy1uD/view?usp=drive_link", width=120)
 
 # Title
 st.title("\U0001F530 Skyline Summit Unit Champion Tracker \U0001F530")
